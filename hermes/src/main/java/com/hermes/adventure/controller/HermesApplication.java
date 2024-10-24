6ecf071101b28199b53f1113e306722aeba047b0
NassarHQ <mnassar.personal@gmail.com>
2024-10-24 09:34:10 -0700

    Implemented  UserController.java

--- hermes/src/main/java/com/hermes/adventure/controller/HermesApplication.java
+++ hermes/src/main/java/com/hermes/adventure/controller/HermesApplication.java
@@ -1,4 +1,4 @@
-package com.hermes.adventure;
+package com.hermes.adventure.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ hermes/src/main/java/com/hermes/adventure/controller/UserController.java
@@ -0,0 +1,59 @@
+package com.hermes.adventure.controller;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Controller class that handle incoming HTTP request
+@RestController
+@RequestMapping("/api/users")
+class UserController {
+
+    private final UserService userService;
+
+    // Constructor to inject UserService
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // HTTP GET request to retrieve all users
+    @GetMapping
+    public ResponseEntity<List<user>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    // HTTP GET request to retrieve a specific user by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    // HTTP POST request to create a new user
+    @PostMapping("/register")
+    publoc ResponseEntity<User> registerUser(@RequestBody User user) {
+        User createdUser = userService.registerUser(user);
+        return ResponseEntity.ok(createdUser);
+    }
+
+    // HTTP PUT request to update a user by ID
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
+        User updatedUser = userService.updateUser(id, user);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    // HTTP DELETE request to delete a user by ID
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity<Void>.noContent().build();
+    }
+}
